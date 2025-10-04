cef4089c06f5b8dd879d194108e4a380d5e490e3
GitHub Action <action@github.com>
2025-10-04 19:28:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/hrgdavor/ideahtmlview/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hrgdavor/ideahtmlview/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hrgdavor.ideahtmlview.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hrgdavor.ideahtmlview.MyBundle
+import com.github.hrgdavor.ideahtmlview.services.MyProjectService
 import javax.swing.JButton
 
 
